a9a788b3ee92e4a013a458f09e1bed8e88304bec
Paul <paul.home47@gmail.com>
2017-10-06 15:43:03 +0100

    reinserting main

---
+++ Main.pl
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+use LWP::Simple;
+use Data::Dumper;
+
+require Data::Employee;
+require DAO::EmployeeDao;
+require DAO::ConnectionDao;
+
+# use Dao::EmployeesDao qw(readEmployees writeEmployees);
+
+
+$| = 1;
+
+sub main {
+
+	my $filePath = "dataFiles/employeeRecords.csv";
+	my %employeesHash = DAO::EmployeeDao::getAllEmployees($filePath);
+	foreach my $emp (keys %employeesHash) {
+		print Dumper($employeesHash{$emp});
+	}
+	
+}
+
+##-----------------------------------------------------------------
+
+
+main();
+
+
